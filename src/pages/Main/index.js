e0b467f278fe43e95299968b96978487790e1b32
marcio-mhs <marciomhs@hotmail.com>
2021-02-19 09:06:49 -0300

    aula 91

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
-import { Title } from './styles';
+import {FaGithub, FaPlus} from 'react-icons/fa';
+import { Container, Form, SubmitButton } from './styles';
 
 export default function Main(){
     return (
-        <Title>
-            Main
-        </Title>
+        <Container>
+            <FaGithub size={25}/>
+            <h1>Meus repositórios</h1>
+
+            <Form onSumit={() => {}}>
+                <input type="text" placeholder="Adicionar Repositórios" />
+                <SubmitButton>
+                    <FaPlus color="#FFF" size={14}/>
+                </SubmitButton>
+            </Form>
+        </Container>
     );
 };
