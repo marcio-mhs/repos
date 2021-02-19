0d0d02109203c6c5ac3884b935dc6b6485293c46
marcio-mhs <marciomhs@hotmail.com>
2021-02-19 16:07:54 -0300

    aula 98

--- src/pages/Repositorio/index.js
+++ src/pages/Repositorio/index.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import api from '../../services/api';
-import { Container } from './styles';
+import { Container, Owner, Loading, BackButton } from './styles';
+import {FaArrowLeft} from 'react-icons/fa';
 
 export default function Repositorio({match}){
 
@@ -14,26 +15,44 @@ export default function Repositorio({match}){
             const nomeRepo = decodeURIComponent(match.params.repositorio);
 
             const [repositorioData, issuesData] = await Promise.all([
-                api.get(`repos/${nomeRepo}`),
-                api.get(`repos/${nomeRepo}/isseus`, {
+                api.get(`/repos/${nomeRepo}`),
+                api.get(`/repos/${nomeRepo}/issues`, {
                     params: {
                         state: 'open',
                         per_page: 5
                     }
                 })
             ]);
-            setRepositorio(repositorioData);
-            setIsseus(issuesData);
+            
+            setRepositorio(repositorioData.data);
+            setIsseus(issuesData.data);
             setLoading(false);
         }
 
         load();
 
     }, [match.params.repositorio]);
-
+    
+    if (loading){
+        return(
+            <Loading>
+                <h1>Carregando!</h1>
+            </Loading>
+        )
+    } 
     return (
         <Container>
-
+            <BackButton to="/">
+                <FaArrowLeft color="#000" size={35}/>
+            </BackButton>
+            <Owner>
+            <img 
+                    src={repositorio.owner.avatar_url} 
+                    alt={repositorio.owner.login} />
+                <h1>{repositorio.name}</h1>
+                <p>{repositorio.description}</p>
+            </Owner>
         </Container>
     );
+          
 };
